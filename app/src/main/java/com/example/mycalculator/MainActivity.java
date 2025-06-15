7400f1faa6682bd8df9c1fa9cf94097e61e409fd
Tarun Kannadas <tarunkannadas@gmail.com>
2025-06-15 20:23:27 +0530

    Completed project with a small logic pending

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton add,subtract,multiply,divide;
 
-    TextView result;
+    TextView result,clear_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +37,54 @@ public class MainActivity extends AppCompatActivity {
         multiply = findViewById(R.id.into);
         divide = findViewById(R.id.divide);
         result = findViewById(R.id.result);
+        clear_text = findViewById(R.id.clear);
+        clear_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num1.setText("");
+                num2.setText("");
+                result.setText("");
+            }
+        });
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int no1 = Integer.parseInt(num1.getText().toString());
                 int no2 = Integer.parseInt(num2.getText().toString());
                 int res = add(no1,no2);
+                result.setText(String.valueOf(res));
+            }
+        });
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int no1 = Integer.parseInt(num1.getText().toString());
+                int no2 = Integer.parseInt(num2.getText().toString());
+                int res = sub(no1,no2);
+                result.setText(String.valueOf(res));
+            }
+        });
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int no1 = Integer.parseInt(num1.getText().toString());
+                int no2 = Integer.parseInt(num2.getText().toString());
+
+                if (no2 == 0) {
+                    Toast.makeText(getApplicationContext(), "Can't Divide by Zero", Toast.LENGTH_SHORT).show();
+                    result.setText("Undefined");
+                } else {
+                    float res = (float) no1 / no2; // force float division
+                    result.setText(String.format("%.2f", res)); // round to 2 decimal places
+                }
+            }
+        });
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int no1 = Integer.parseInt(num1.getText().toString());
+                int no2 = Integer.parseInt(num2.getText().toString());
+                int res = mul(no1,no2);
                 result.setText(String.valueOf(res));
             }
         });
@@ -52,7 +95,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void add(int no1, int no2){
+    private int add(int no1, int no2){
         return no1+no2;
+    }
+
+    private int sub(int no1, int no2){
+        return no1-no2;
+    }
+
+//    private int div(int no1, int no2){
+//        if (no2 == 0)
+//        {
+//            Toast.makeText(getApplicationContext(),"Can't Divide by Zero", Toast.LENGTH_SHORT).show(); //to avoid division by zero
+//            return 0;
+//        }
+//        return no1/no2;
+//    }
+
+    private int mul(int no1, int no2){
+        return no1*no2;
     }
 }
